36034b16ae4075cf7f8d36077463445635d7955d
Baymint689 <paulus.bayu12@gmail.com>
2022-09-15 08:52:46 +0700

    View Binding

---
+++ app/src/main/java/com/example/view_binding_10691/TaskList.kt
@@ -0,0 +1,12 @@
+package com.example.view_binding_10691
+
+object TaskList {
+    val taskList = listOf<Task> (
+        Task("Chapter 1", "Bangun Tidur", "Kebiasaan yang membuat hari semakin indah"),
+        Task("Chapter 2", "Ngedaily Genshin", "Mencari Primo buat gacha Tighnari"),
+        Task("Chapter 3", "Santai di cafe", "Waktu luang enaknya nyantai di cafe"),
+        Task("Chapter 4", "Sumeru oh Sumeru", "Ngelanjutin side quest Sumeru"),
+        Task("Chapter 5", "Upload ke Tiktok", "Topik: Cerita Sumeru Secara Singkat")
+
+    )
+}
